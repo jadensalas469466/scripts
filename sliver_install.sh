34195787b4014c145f2e2d2047aba71c2787cc15
sec <173543555+jadensalas469466@users.noreply.github.com>
2025-09-23 16:58:03 +0800

    update

--- sliver_install.sh
+++ sliver_install.sh
@@ -141,7 +141,7 @@ sudo -E systemctl start sliver-server.service
 # Generate local config
 echo "Generating config ..."
 mkdir -p "$HOME/.sliver"
-# sliver-server operator -n "$(whoami)" -l localhost -p 31337 -s "$HOME/.sliver"
+sliver-server operator -n "$(whoami)" -l localhost -p 31337 -s "$HOME/.sliver"
 chown -R "$(whoami)":"$(whoami)" "$HOME/.sliver"
 
 echo "sliver has been successfully installed to $HOME/.local/bin."
