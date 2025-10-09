911db94f9bce954f59e3ee356bc4b141ada6a2f8
sec <173543555+jadensalas469466@users.noreply.github.com>
2025-10-09 15:20:49 +0800

    update

---
+++ LocalSend.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 错误检测
+set -e
+
+# 切换到家目录
+cd "$HOME"
+
+# 获取 LocalSend 的最新版本
+echo "Fetching latest LocalSend release URLs..."
+ARTIFACTS=$(curl -fs "https://api.github.com/repos/localsend/localsend/releases/latest" \
+    | awk -F '"' '$2 == "browser_download_url" { print $4 }')
+
+# 下载 LocalSend 的最新版本
+echo "$ARTIFACTS" | while read -r URL; do
+
+    if [[ "$URL" == *LocalSend-*-linux-x86-64.deb ]]; then
+        echo "Downloading LocalSend"
+        curl -fLO "$URL"
+    fi
+done
+
+# 安装 LocalSend
+echo "Installing LocalSend..."
+sudo gdebi -n LocalSend-*-linux-x86-64.deb
+trash LocalSend-*-linux-x86-64.deb
+
+echo "LocalSend has been successfully installed."
